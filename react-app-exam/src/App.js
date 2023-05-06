0933dbf23cbd4f929f7b13dbdc21eaa3b203fe74
Marie <marie.pannier@smail.th-koeln.de>
2023-05-06 20:51:34 +0200

    colorchange

--- react-app-exam/src/App.js
+++ react-app-exam/src/App.js
@@ -44,6 +44,7 @@ function App() {
             date: new Date(obj["date"]).toLocaleString(),
           };
         });
+        //toFixed rundet auf die zweite Nachkommastelle
         setTempSeries(chartData.map((obj) => obj.temp.toFixed(2)));
         setHumSeries(chartData.map((obj) => obj.hum.toFixed(2)));
         setLightSeries(chartData.map((obj) => obj.light));
@@ -61,18 +62,24 @@ function App() {
 
   // ApexCharts needs some default options to know what to show
   const options = {
+    colors: ['#ff6347', '#7ac5cd', '#ffb90f'],
     chart: {
       height: 350,
       type: "area",
       background: "#f5f5f5",
-      fontFamily: "Arial, sans-serif",
+      fontFamily: "Helvetica,Arial, sans-serif",
       foreColor: "grey",
     },
     dataLabels: {
       enabled: true,
     },
     stroke: {
-      curve: "smooth",
+      show: true,
+      curve: 'smooth',
+      lineCap: 'butt',
+      colors: undefined,
+      width: 0,
+      dashArray: 0,
     },
     noData: {
       text: "Loading...",
@@ -81,7 +88,7 @@ function App() {
         style: {
           fontSize: "14px",
           fontWeight: 600,
-          fontFamily: "Arial, sans-serif",
+          fontFamily: "Helvetica,Arial, sans-serif",
           color: "#333333",
         },
       categories: dates,
@@ -90,7 +97,7 @@ function App() {
         style: {
           fontSize: "14px",
           fontWeight: 600,
-          fontFamily: "Arial, sans-serif",
+          fontFamily: "Helvetica,Arial, sans-serif",
           color: "#333333",
             
         },
@@ -100,10 +107,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Chart options={{ ...options, title: { text: "All values" } }} series={dataSeries} type="area" height={350} />
-      <Chart options={{ ...options, title: { text: "Temperature" } }} series={[{ name: "Temperature", data: tempSeries }]} type="area" height={350} />
-      <Chart options={{ ...options, title: { text: "Humidity" } }} series={[{ name: "Humidity", data: humSeries }]} type="area" height={350} />
-      <Chart options={{ ...options, title: { text: "Light" } }} series={[{ name: "Light", data: lightSeries }]} type="area" height={350} />
+      <Chart options={{ ...options, title: { text: "All values" }}} series={dataSeries} type="area" height={350} />
+      <Chart options={{ ...options, title: { text: "Temperature" },colors: ['#ff6347'], }} series={[{ name: "Temperature", data: tempSeries }]} type="area" height={350}/>
+      <Chart options={{ ...options, title: { text: "Humidity" },colors: ['#7ac5cd'], }} series={[{ name: "Humidity", data: humSeries }]} type="area" height={350} />
+      <Chart options={{ ...options, title: { text: "Light" },colors: ['#ffb90f'], }} series={[{ name: "Light", data: lightSeries }]} type="area" height={350} />
     </div>
  
   );
